d3b87a04aed4733087639b48b6dfcf327bc646c9
Travis Edward <Travis Edward@TECH-PC>
2020-02-13 03:47:17 -0400

    Bootsrap for Login and Registration
    Attempt to install multiple bootstraps to select best. login and
    registration files, working login function with bootstrap. Fixed all
    necessary design issues. Currently working on registration page login
    with bootrap for user design.

--- registration.php
+++ registration.php
@@ -1,44 +1,146 @@
 
-<?php 
-
-	if(isset($_POST["registration"])){
-
-include "connection.php";
-    $user_id = $_POST["user_id"];
-	$first_name = $_POST["first_name"];
-	$last_name = $_POST["last_name"];
-	$email = $_POST["email"];
-	$password = $_POST["password"];
-
-	$password = password_hash($password, PASSWORD_DEFAULT);
-
-	//////////////////////////////////////////////////////////////
-
-	$sql = "INSERT INTO `users` (`user_id`, `first_name`, `last_name`, `email`, `password`, `regdate`) 
-	VALUES ('NULL', '$first_name', '$last_name', '$email', '$password', current_timestamp());";
 
 
-	if (mysqli_query($conn, $sql)) {
-		echo "New record created successfully";
-	} else {
-		echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-	}
 
-}
+
+<?php
+
+
 
 include 'header.php'; ?>
 
-  	<h1>Student Registration</h1>
+<style>
 
-	<form action="registration.php" method= "post">
 
-	<p>First Name: <input type="text" name="first_name"></p>
-	<p>Last Name: <input type="text" name="last_name"></p>
-	<p>Email: <input type="text" name="email"></p>
-	<p>Password: <input type="text" name="password"></p>
+.divider-text {
+    position: relative;
+    text-align: center;
+    margin-top: 15px;
+    margin-bottom: 15px;
+}
+.divider-text span {
+    padding: 7px;
+    font-size: 12px;
+    position: relative;   
+    z-index: 2;
+}
+.divider-text:after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    border-bottom: 1px solid #ddd;
+    top: 55%;
+    left: 0;
+    z-index: 1;
+}
 
-	<input type="submit" name="registration" value="Register">
+.input-group-prepend span{
+width: 50px;
+background-color: #FFC312;
+color: black;
+border:0 !important;
+}
+.btn-facebook {
+    background-color: #FFC312;
+    color: black;
+}
+.btn-twitter {
+    background-color: #FFC312;
+    color: black;
+}
 
-	</form>
+.btn-primary{
+     background-color: #FFC312;
+    color: black;
+    border: none;
+}
+.btn-facebook:hover{
+color: black;
+background-color: #3b5998;
+}
+.btn-twitter:hover{
+color: black;
+background-color: #00acee;
+}
+.btn-primary:hover{
+color: black;
+background-color: #39C16C;
+}
+.card{
+height: 100%;
+margin-top: auto;
+margin-bottom: auto;
+width: 100%;
+background-color: rgba(0,0,0,0.5) !important;
+}
+</style>
 
-<?php include 'footer.php';?>
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+
+<br>
+<br>
+<div class="container" >
+<div class="card bg-light">
+<article class="card-body mx-auto" style="max-width: 400px;">
+	<h4 class="card-title mt-3 text-center">Create Account</h4>
+	<p class="text-center">Get started with your free account</p>
+	<p>
+		<a href="" class="btn btn-block btn-twitter"> <i class="fab fa-twitter"></i>   Login via Twitter</a>
+		<a href="" class="btn btn-block btn-facebook"> <i class="fab fa-facebook-f"></i>   Login via facebook</a>
+	</p>
+	<p class="divider-text">
+        <span class="bg-light">OR</span>
+    </p>
+	<form>
+	<div class="form-group input-group">
+		<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+		 </div>
+        <input name="first_name" class="form-control" placeholder="First Name" type="text">
+    </div> <!-- form-group// -->
+    <div class="form-group input-group">
+		<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+		 </div>
+        <input name="last_name" class="form-control" placeholder="Last Name" type="text">
+    </div> <!-- form-group// -->
+    <div class="form-group input-group">
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
+		 </div>
+        <input name="email" class="form-control" placeholder="Email address" type="email">
+    </div> <!-- form-group// -->
+    <div class="form-group input-group">
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-building"></i> </span>
+		</div>
+		<select class="form-control">
+			<option selected="">Employment status</option>
+			<option>Employed</option>
+			<option>Unemployed</option>
+		</select>
+	</div> <!-- form-group end.// -->
+    <div class="form-group input-group">
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+		</div>
+        <input  name="password" class="form-control" placeholder="Create password" type="password">
+    </div> <!-- form-group// -->
+    <div class="form-group input-group">
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+		</div>
+        <input name="password" class="form-control" placeholder="Repeat password" type="password">
+    </div> <!-- form-group// -->                                      
+    <div class="form-group">
+        <button name="registration" type="submit" class="btn btn-primary btn-block" value="register" > Create Account  </button>
+    </div> <!-- form-group// -->      
+    <p class="text-center">Have an account? <a href="login.php">Log In</a> </p>                                                                 
+</form>
+</article>
+</div> <!-- card.// -->
+</div>
+<br><br>
+</article>
+<?php include 'footer.php'; ?>
